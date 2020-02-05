bb402f68dcf092926176ebf61af8e8f2925bb2f0
sdmalek44 <malek.stephen44@gmail.com>
2020-02-05 06:40:17 -0700

    Add update method to project controller to invoke some action cable side effect after updating project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,23 @@
 class ProjectsController < ApplicationController
   def index;end
+
+  def update
+    if @project.update(project_params)
+      invoke_cables
+      redirect_back(
+        fallback_location: root_path,
+        notice: 'Project was successfully updated.'
+      )
+    end
+  end
+
+  private
+
+  def invoke_cables
+    CableServices::NotifyJobService.new(
+      project: @project,
+      action: action_name.to_sym,
+      user: current_user
+    )
+  end
 end
